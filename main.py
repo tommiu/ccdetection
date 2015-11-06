293e257e07d35a5ecd30a4c28bd7fe1c856330d6
Tommi <tommiu@web.de>
2015-11-06 02:02:41 +0100

    Some additions and changed traversePath() to be a generator. This way, a huge list of directories can be processed one by one, instead of processing the whole list first.

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ from manual_search import ManualCCSearch
 from args_parser import ModeArgsParser
 import pexpect
 import os
+import signal
+import subprocess
 
 
-ARGS_HELP = "help"
-ARGS_SEARCH = "search"
+ARGS_HELP    = "help"
+ARGS_SEARCH  = "search"
+ARGS_CONSOLE = "console"
+
+SPAWN_SCRIPT = "/opt/phpjoern/spawn_neodb.sh"
 
 def main(argv):
     # Setup command line arguments.
@@ -31,9 +36,24 @@ def main(argv):
     if flow[parser.KEY_MODE] == ARGS_HELP:
         parser.printHelp(argv[0])
         sys.exit()
-        
+    
+    elif flow[parser.KEY_MODE] == ARGS_CONSOLE:
+        startConsoleMode(os.path.abspath(flow["in"]))
+    
     elif flow[parser.KEY_MODE] == ARGS_SEARCH:
         flow["in"] = os.path.abspath(flow["in"])
+        code_path  = ""
+        code = ""
+        
+        try:
+            code_path = flow["c"]
+        except:
+            code_path = flow["code"]
+        
+        # Read given code.
+        with open(code_path, "r") as fh:
+            code = fh.read()
+        
         if "l" in flow or "level" in flow:
             level = 0
             if "l" in flow:
@@ -42,22 +62,25 @@ def main(argv):
                 level = int(flow["level"])
                 
             try:
-                paths = getRootDirectories(flow["in"], level)
+                path_generator = getRootDirectories(flow["in"], level)
                 
             except Exception as err:
                 print "An exception occured: %s" % err
                 sys.exit()
                 
-            if paths:
-                for path in paths:
-                    analyseData(path)
+#             if paths:
+#                 print "Found %d projects. Going to analyse them now." % (
+#                                                                     len(paths)
+#                                                                     )
+            for path in path_generator:
+                analyseData(code, path)
                 
             else:
                 print "There is no top-level directory %d levels below '%s'" %(
                                     level, flow["in"]
                                     )
         else:
-            analyseData(flow["in"])
+            analyseData(code, flow["in"])
 
     else:
         parser.printHelp(argv[0])
@@ -84,9 +107,10 @@ def getRootDirectories(path, level):
                 )
     
     root_directories = []
-    traversePath(root_directories, path, 1, level)
+#     traversePath(root_directories, path, 1, level)
     
-    return root_directories
+#     return root_directories
+    return traversePath(root_directories, path, 1, level)
 
 def traversePath(destination_list, path, current_level, target_level):
     """
@@ -100,35 +124,71 @@ def traversePath(destination_list, path, current_level, target_level):
         if os.path.isdir(_path):
             if current_level == target_level:
                 # This path is a directory and on the desired level - append it.
-                destination_list.append(_path)
+                yield _path
+#                 destination_list.append(_path)
                 
             else:
                 # Traverse directory deeper.
-                traversePath(destination_list, _path, current_level + 1, target_level)
+                traversePath(
+                        destination_list, _path, 
+                        current_level + 1, target_level
+                        )
 
-def analyseData(path):
+def analyseData(code, path):
     """
     Create the PHP AST of the files in 'path', import them into the 
     neo4j graph database and start the neo4j console.
     Finally, run the analysing query against the graph database. 
     """
-    process = prepareData(path)
-    
-    cc_tester = ManualCCSearch()
-    cc_tester.runTimedQuery(cc_tester.sqlNewIndirect)
-    
-    process.sendcontrol('c')
-    process.close()    
+    try:
+        process = prepareData(path)
+            
+        cc_tester = ManualCCSearch()
+        cc_tester.runTimedQuery(cc_tester.runQuery, query=code)
+        
+        process.sendcontrol('c')
+        process.close()    
+        
+    except BindException as err:
+        print err
+        print "Trying 'pkill -f java -cp.+neo4j' and restart"
+        process = pexpect.run("pkill -f \"java -cp.+neo4j\"")
+        analyseData(code, path)
 
 def prepareData(path):
     print "Analysing path: %s" % path
 #     process = pexpect.spawn("/bin/bash", ["/opt/phpjoern/spawn_neodb.sh", path], 15)
-    process = pexpect.spawn("/opt/phpjoern/spawn_neodb.sh", [path,], 15)
-    process.expect(["graph.db still exists", "Remote interface ready"])
-    print process.before
+    process = pexpect.spawn(SPAWN_SCRIPT, [path,], 360)
+    expectation = process.expect([
+                        "graph.db still exists", 
+                        "Remote interface ready", 
+                        "java.net.BindException"
+                        ])
+    
+    if expectation == 2:
+#         print process.before
+        raise BindException()
     
     return process
+
+def startConsoleMode(path):
+    """
+    Import the php file/project AST from 'path' into the neo4j 
+    database and start the neo4j console, using the 'SPAWN_SCRIPT' file.
+    """
+#     print "Using path %s" % path
+    process = subprocess.call(
+                        [SPAWN_SCRIPT, path],
+#                         shell=True,
+                        preexec_fn=os.setsid
+                        )
     
+    def signalHandler(signalnum, handler):
+        os.killpg(process.pid, signal.SIGINT)
+        
+    signal.signal(signal.SIGINT, signalHandler)
+    signal.signal(signal.SIGTERM, signalHandler)
+            
 def setupArgs(parser):
     """
     Setup command line arguments combinations.
@@ -153,9 +213,36 @@ def setupArgs(parser):
                             ["c=", "code"],
                             ["in=", None]
                             ],
-                                [["l=", "level"]],
+                                [
+                            ["l=", "level"]
+                            ],
+                                explanation=explanation
+                                )
+    
+    # Open neo4j console: console -in dir/file
+    explanation = (
+                "Import the php file/project AST into the neo4j database and "
+                "start the neo4j console."
+                )
+    parser.addArgumentsCombination(
+                                ARGS_CONSOLE,
+                                [["in=", None]],
                                 explanation=explanation
                                 )
 
+class BindException(BaseException):
+    def __init__(self, msg=None):
+        if msg:
+            self.message = msg
+            
+        else:
+            self.message = (
+                    "java.net.BindException: Address already in use. "
+                    "-> pkill neo4j."
+                    )
+            
+    def __str__(self):
+        return self.message
+            
 if __name__ == '__main__':
     main(sys.argv)
