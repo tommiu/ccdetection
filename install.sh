23120f0aa1d93651c07e636ea7fdbf584b099fb8
Tommi <tommiu@web.de>
2015-11-18 02:50:52 +0100

    improvements

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@
 # E.g. to install OpenJDK 7: sudo apt-get install openjdk-7-jdk
 
 function informRestart {
-	echo "Install failed at step $3. Restart using following command:"
-	echo "./install.sh $1 $2 $3"
+	echo "Install failed at step `expr $3 - 1`. Restart using following command:"
+	echo "./install.sh $1 $2 `expr $3 - 1`"
 }
 
 if [ -z "$1" ]; then
@@ -37,7 +37,9 @@ CPUS=$2
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if (("$SKIP" == "0")) ; then
+_CTR=0
+
+if (("$SKIP" == "$_CTR")) ; then
 sudo apt-get update
 # Following tools will be used to install everything:
 sudo apt-get install autoconf bison flex maven unzip libxml2-dev python-setuptools python-dev -y
@@ -46,64 +48,80 @@ fi
 
 cd $INSTALL_PATH
 
-if (("$SKIP" < "2")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 # Get phpjoern and python-joern
 if ! git clone ssh://git@service.cispa.uni-saarland.de:2222/phpjoern.git
-	then
-	informRestart $1 $2 1
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "3")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! git clone ssh://git@service.cispa.uni-saarland.de:2222/python-joern.git
-	then
-	informRestart $1 $2 2
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
+
+cd python-joern
 
 # Get PHPJoernSteps extension for python-joern.
-cd python-joern
-git checkout portPHPJoern
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! git checkout portPHPJoern
+then
+	informRestart $1 $2 $_CTR
+	exit
+fi
+fi
 
 cd $INSTALL_PATH
 
 # Install custom gremlin steps for python-joern.
-cp -r $THIS_SCRIPT_DIR/custom_gremlin_steps $INSTALL_PATH/python-joern/joern/phpjoernsteps/.
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! cp -r $THIS_SCRIPT_DIR/custom_gremlin_steps $INSTALL_PATH/python-joern/joern/phpjoernsteps/.
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
 
-if (("$SKIP" < "4")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 # Download PHP7 - ~160 MB
 if ! mkdir php7
-	then
-	informRestart $1 $2 3
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if ! cd $INSTALL_PATH/php7
-	then
-	exit
-fi
+cd $INSTALL_PATH/php7
 
-if (("$SKIP" < "5")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! git clone https://git.php.net/repository/php-src.git
-	then
-	informRestart $1 $2 4
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 cd php-src
 
-if (("$SKIP" < "6")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 cd ext
 if ! git clone https://github.com/nikic/php-ast.git ast
-	then
-	informRestart $1 $2 5
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
-
 cd ..
 fi
 
@@ -114,134 +132,181 @@ fi
 # Example error: "configure: error: xml2-config not found. Please check your libxml2 installation."
 # Solution: sudo apt-get install libxml2-dev
 # etc. for every error encountered.
-if (("$SKIP" < "7")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! ./buildconf
-	then
-	informRestart $1 $2 6
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (( "$SKIP" < "8" )) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! ./configure --prefix=$INSTALL_PATH/php7/usr --with-config-file-path=$INSTALL_PATH/php7/usr/etc --enable-ast
-	then
-	informRestart $1 $2 7
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
+
 # If make fails because of missing .h or .c files, then you encountered a bug of buildconf/configure.
 # The solution is to manually look for the line inside the Makefile, which should produce the missing file. Use the specified command manually to build the missing file. Afterwards retry 'make'.
-if (("$SKIP" < "9")) ; then
+# Example error: File Zend/zend_language_parser.y is missing.
+# Find rule in Makefile, cd into dir 'Zend' and execute the make rule: bison -y -p zend -v -d /path_to_php/php7/php-src/Zend/zend_language_parser.y -o zend_language_parser.c
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! make -j $CPUS
-	then
-	informRestart $1 $2 8
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "10")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! make install
-	then
-	informRestart $1 $2 9
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 # Replace php7 php.ini with php.ini from phpjoern.
-cp $1/phpjoern/conf/php.ini $1/php7/usr/etc/php.ini
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! cp $1/phpjoern/conf/php.ini $1/php7/usr/etc/php.ini
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
 
 # PHP7 install finished.
 # Next: Install the Neo4j 2.1 graph database server.
-if (("$SKIP" < "11")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! mkdir $NEO4J_BASEDIR
-	then
-	informRestart $1 $2 10
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 cd /tmp/
 
-if (("$SKIP" < "12")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! curl -O http://neo4j.com/artifact.php?name=neo4j-community-2.1.8-unix.tar.gz
-	then
-	informRestart $1 $2 11
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "13")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! tar xvfz artifact.php\?name=neo4j-community-2.1.8-unix.tar.gz
-	then
-	informRestart $1 $2 12
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "14")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! mv neo4j-community-2.1.8 $NEO4J_BASEDIR/neo4j-01
-	then
-	informRestart $1 $2 13
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 # Next: Install the Gremlin plugin for Neo4j 2.1.
-if (("$SKIP" < "15")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! git clone https://github.com/neo4j-contrib/gremlin-plugin
-	then
-	informRestart $1 $2 14
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 cd gremlin-plugin
 
-if (("$SKIP" < "16")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! mvn clean package
-	then
-	informRestart $1 $2 15
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "17")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! unzip target/neo4j-gremlin-plugin-2.1-SNAPSHOT-server-plugin.zip -d $NEO4J_BASEDIR/neo4j-01/plugins/gremlin-plugin
-	then
-	informRestart $1 $2 16
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 # Copy the neo4j installation to three other places, so that we can use four different neo4j instances at once.
-cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-02
-cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-03
-cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-04
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-02
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
+
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-03
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
+
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! cp -r $NEO4J_BASEDIR/neo4j-01 $NEO4J_BASEDIR/neo4j-04
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
 
 # Next: Install batch-import 2.1 (should match neo4j version). 
 # This is needed to import graph databases into neo4j 2.1.
-if (("$SKIP" < "18")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! mkdir $INSTALL_PATH/batch-import
-	then
-	informRestart $1 $2 17
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
 cd /tmp
 
-if (("$SKIP" < "19")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! curl -O https://dl.dropboxusercontent.com/u/14493611/batch_importer_21.zip
-	then
-	informRestart $1 $2 18
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
 
-if (("$SKIP" < "20")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! unzip batch_importer_21.zip -d $INSTALL_PATH/batch-import
-	then
-	informRestart $1 $2 19
+then
+	informRestart $1 $2 $_CTR
 	exit
 fi
 fi
@@ -250,27 +315,52 @@ fi
 #SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install python-joern globally.
-if (("$SKIP" < "21")) ; then
-wget https://github.com/fabsx00/python-joern/archive/0.3.1.tar.gz
-tar xfzv 0.3.1.tar.gz
-cd python-joern-0.3.1
-sudo python2 setup.py install
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! wget https://github.com/fabsx00/python-joern/archive/0.3.1.tar.gz
+then
+    informRestart $1 $2 $_CTR
+    exit
 fi
+fi
+
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! tar xfzv 0.3.1.tar.gz
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
+
+cd python-joern-0.3.1
+
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
+if ! sudo python2 setup.py install
+then
+    informRestart $1 $2 $_CTR
+    exit
+fi
+fi
+
 # Utilities installation DONE.
 # Configure ccdetection with installed paths.
-if (("$SKIP" < "22")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! python $THIS_SCRIPT_DIR/main.py config -p $INSTALL_PATH
-    then
-    informRestart $1 $2 21
+then
+    informRestart $1 $2 $_CTR
     exit
 fi
 fi
 
 # Link neo4j databases
-if (("$SKIP" < "23")) ; then
+_CTR=`expr $_CTR + 1`
+if (("$SKIP" < "$_CTR")) ; then
 if ! ln -s $THIS_SCRIPT_DIR/graphs/graph1.db $NEO4J_BASEDIR/neo4j-01/data/graph.db || ! ln -s $THIS_SCRIPT_DIR/graphs/graph2.db $NEO4J_BASEDIR/neo4j-02/data/graph.db || ! ln -s $THIS_SCRIPT_DIR/graphs/graph3.db $NEO4J_BASEDIR/neo4j-03/data/graph.db || ! ln -s $THIS_SCRIPT_DIR/graphs/graph4.db $NEO4J_BASEDIR/neo4j-04/data/graph.db
-    then
-    informRestart $1 $2 22
+then
+    informRestart $1 $2 $_CTR
     exit
 fi
 fi
