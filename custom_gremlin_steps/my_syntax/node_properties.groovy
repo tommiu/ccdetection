d2359e8ee4a800100909aa59722429ed9df59804
Tommi <tommiu@web.de>
2016-01-31 19:30:56 +0100

    fix for AST_EDGE bug?

--- custom_gremlin_steps/my_syntax/node_properties.groovy
+++ custom_gremlin_steps/my_syntax/node_properties.groovy
@@ -1,14 +1,3 @@
-/**
-   Return all nodes in the subtrees rooted in the current vertices and
-   filter them according to a predicate p.
-
-   @param p The predicate
-*/
-/*Gremlin.defineStep('hasFlag', [Vertex, Pipe], { p ->
-  _().filter{ it.flags[0] == p }
-})
-*/
-
 Gremlin.defineStep('hasFlag', [Vertex, Pipe], { p ->
   _().filter{ 
                 def x = false;
@@ -23,3 +12,5 @@ Gremlin.defineStep('hasFlag', [Vertex, Pipe], { p ->
 Object.metaClass.isType = { it, type ->
     it.filter{ it.getProperty("type") == type }.count() == 1
 }
+
+AST_EDGE = 'PARENT_OF'
